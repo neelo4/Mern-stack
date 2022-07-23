b1eb09aa1a12268b445ad581bd66f2b8627f82d0
neelofar <neelo4khan@gmail.com>
2022-07-23 20:15:33 +0530

    controller nad router setup

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,22 +1,31 @@
+
+const asyncHandler = require('express-async-handler');
+
 //rather than creating multiple functions we can create a single function here in controller file and use in router file 
 // @desc get goals
 // @route
 // @access Private
-const getGoals = (req,res) => {
-    res.status(200).json({message: 'Get goals'})
-}
+const getGoals = asyncHandler(async (req,res) => {
+   res.status(200).json({message: 'Get goals'})
+})
 
-const setGoals = (req,res) => {
-    conssole.log(req.body);
+const setGoals = asyncHandler(async (req,res) => {
+
+    if(!req.body.text){
+      res.status(400)
+      throw new Error('please add a text field')
+
+    }
+
     res.status(200).json({message: 'set goals'})
-}
+})
 
-const updateGoals = (req,res) => {
+const updateGoals = asyncHandler(async (req,res) => {
     res.status(200).json({message: `Update goals ${req.params.id}`})
-}
-const deleteGoals = (req,res) => {
+})
+const deleteGoals = asyncHandler(async (req,res) => {
     res.status(200).json({message: `Delete goals ${req.params.id}`})
-}
+})
 
 module.exports = {
     getGoals,
